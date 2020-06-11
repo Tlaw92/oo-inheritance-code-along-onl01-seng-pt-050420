780b5339f80de157cdc9de9ff45575554d032db7
travis Lawson <travislawclimb@gmail.com>
2020-06-11 06:11:25 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,4 +1,13 @@
 require_relative "./vehicle.rb"
 
+class Car 
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end   
+  
+  
+end   
 
 
